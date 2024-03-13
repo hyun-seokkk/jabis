31fde7c7198ac4644c7f9d3dc663e1b821e39b57
최호연 <hy8463@naver.com>
2024-03-13 15:56:42 +0900

    Design: 회원가입 페이지 구성

--- frontend/src/views/Users/SignupView.vue
+++ frontend/src/views/Users/SignupView.vue
@@ -2,35 +2,56 @@
     <div class="section">
         <div class="container">
             <div class="title">회원가입</div>
-            <form @submit.prevent="submitForm">
+            <form @submit.prevent="signUp">
                 <div class="input">
                     <div class="title2">ID</div>
                     <input
                         class="textfield"
                         type="text"
                         v-model="userId"
-                        placeholder="Enter your ID" />
+                        placeholder="ID를 입력하세요" />
+                    <span v-if="idValid" class="error-message"
+                        >아이디는 영문, 숫자를 포함한 4자이상 20자 이내여야 합니다.</span
+                    >
+                    {{ idValid }}
                 </div>
                 <div class="input">
                     <div class="title2">Password</div>
                     <input
                         class="textfield"
                         type="password"
-                        v-model="password"
-                        placeholder="Enter your password" />
+                        v-model="password1"
+                        placeholder="비밀번호를 입력하세요" />
+                    <span v-if="passwordValid" class="error-message"
+                        >비밀번호는 영문 + 숫자를 포함한 4자이상 20자 이내여야 합니다.</span
+                    >
                 </div>
                 <div class="input">
-                    <div class="title2">Nickname</div>
+                    <div class="title2">Password 확인</div>
+                    <input
+                        class="textfield"
+                        type="password"
+                        v-model="password2"
+                        placeholder="비밀번호를 한번 더 입력하세요" />
+                    <span v-if="passwordSamevalid" class="error-message"
+                        >비밀번호가 일치하지 않습니다.</span
+                    >
+                </div>
+                <div class="input">
+                    <div class="title2">닉네임</div>
                     <input
                         class="textfield"
                         type="text"
                         v-model="nickname"
-                        placeholder="Enter your nickname" />
+                        placeholder="닉네임을 입력하세요" />
+                    <span v-if="nickNamevalid" class="error-message"
+                        >최소 두 글자 이상 입력해주세요.</span
+                    >
                 </div>
                 <div class="button">
-                    <div class="primary">
-                        <button type="submit" class="title3">Sign Up</button>
-                    </div>
+                    <button class="primary" type="submit" @submit="signUp">
+                        <div class="title3">Sign Up</div>
+                    </button>
                 </div>
             </form>
         </div>
@@ -48,13 +69,35 @@
 
 <script setup>
 import { ref } from 'vue';
-const userId = ref('');
-const password = ref('');
-const nickname = ref('');
+import { useCounterStore } from '@/stores/counter';
+import { watch } from 'vue';
+const store = useCounterStore();
+const userId = ref(null);
+const password1 = ref(null);
+const password2 = ref(null);
+const nickname = ref(null);
 
-const submitForm = () => {
-    // 여기에 폼 제출 처리 로직을 추가하세요.
+const signUp = () => {
+    // 회원가입 로직
+    const payload = {
+        userId: userId.value,
+        password1: password1.value,
+        password2: password2.value,
+        nickname: nickname.value,
+    };
+    store.signUp(payload);
 };
+
+const idValid = ref(true);
+const passwordValid = ref(true);
+const passwordSamevalid = ref(true);
+const nickNamevalid = ref(true);
+
+// ID 유효성 검사
+const watchId = watch(userId, (newId, oldId) => {
+    const regex = /^[a-zA-Z0-9]{4,20}$/;
+    idValid.value = regex.test(newId);
+});
 </script>
 
 <style scoped>
