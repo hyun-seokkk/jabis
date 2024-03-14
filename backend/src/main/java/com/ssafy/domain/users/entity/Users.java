2cffa17def28e58c0403c37b90e1da6638d44121
nhs503601 <nhs503601@gmail.com>
2024-03-14 11:21:44 +0900

    Feat: user dto추가, todto메서드 추가

--- backend/src/main/java/com/ssafy/domain/users/entity/Users.java
+++ backend/src/main/java/com/ssafy/domain/users/entity/Users.java
@@ -1,5 +1,6 @@
 package com.ssafy.domain.users.entity;
 
+import com.ssafy.domain.users.dto.UserDto;
 import jakarta.persistence.*;
 import lombok.Getter;
 
@@ -17,5 +18,13 @@ public class Users {
     private String password;
 
     private String name;
+
+    public UserDto toUserDto(Users users) {
+        return UserDto.builder()
+                .id(users.getId())
+                .email(users.getEmail())
+                .name(users.getName())
+                .build();
+    }
 
 }
