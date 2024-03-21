b8999ac9f2f1044eb1f1d069aebc9b3e36403da0
정준수 <junsjung137@gmail.com>
2024-03-22 01:07:56 +0900

    Feat: Add News Dto
    - 2024-03-22
    - Request, Response

--- backend/src/main/java/com/ssafy/domain/news/dto/response/NewsResponse.java
+++ backend/src/main/java/com/ssafy/domain/news/dto/response/NewsResponse.java
@@ -1,2 +1,20 @@
-package com.ssafy.domain.news.dto.response;public class NewsResponse {
+package com.ssafy.domain.news.dto.response;
+
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
+public class NewsResponse {
+
+    private Long newsId;
+
+    private String title;
+
+    private String content;
+
+    private String date;
+
+    private String link;
+
 }
