356cf01c6c756fe498c32a51da59fc906511be7d
정준수 <junsjung137@gmail.com>
2024-03-22 01:05:45 +0900

    Feat: Add Company Dto
    - 2024-03-22
    - CompanyResponse, CompanyYouthResponse

--- backend/src/main/java/com/ssafy/domain/company/dto/response/CompanyResponse.java
+++ backend/src/main/java/com/ssafy/domain/company/dto/response/CompanyResponse.java
@@ -1,2 +1,38 @@
-package com.ssafy.domain.company.dto.response;public class CompanyResponse {
+package com.ssafy.domain.company.dto.response;
+
+import com.ssafy.common.embeddable.Factor;
+
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
+public class CompanyResponse {
+
+    private Long companyId;
+
+    private String name;
+
+    private String foundAt;
+
+    private String owner;
+
+    private String form;
+
+    private Long capital;
+
+    private int employeeCnt;
+
+    private String type;
+
+    private String productName;
+
+    private String address;
+
+    private Factor factor;
+
+    private boolean youthCompany;
+
+    private Long views;
+
 }
