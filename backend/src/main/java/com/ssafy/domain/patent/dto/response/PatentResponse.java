6b270d7d5af254d536dbdf61bb55e8179e937654
정준수 <junsjung137@gmail.com>
2024-03-22 01:08:09 +0900

    Feat: Add Patent Dto
    - 2024-03-22
    - Response

--- backend/src/main/java/com/ssafy/domain/patent/dto/response/PatentResponse.java
+++ backend/src/main/java/com/ssafy/domain/patent/dto/response/PatentResponse.java
@@ -1,2 +1,20 @@
-package com.ssafy.domain.patent.dto.response;public class PatentResponse {
+package com.ssafy.domain.patent.dto.response;
+
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
+public class PatentResponse {
+
+    private Long patentId;
+
+    private int companyId;
+
+    private String patentName;
+
+    private String patentDate;
+
+    private String patentSummary;
+
 }
