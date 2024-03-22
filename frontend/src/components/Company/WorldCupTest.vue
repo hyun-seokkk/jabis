3491754c4c74979cc03582e1e612b416663d9ac2
최호연 <hy8463@naver.com>
2024-03-22 15:02:23 +0900

    Design: 메인 페이지 수정

---
+++ frontend/src/components/Company/WorldCupTest.vue
@@ -0,0 +1,36 @@
+<template>
+    <div></div>
+</template>
+
+<script setup>
+import { ref } from 'vue';
+
+const companies = [
+    '기업 A',
+    '기업 B',
+    '기업 C',
+    '기업 D',
+    '기업 E',
+    '기업 F',
+    '기업 G',
+    '기업 H',
+    '기업 I',
+    '기업 J',
+    '기업 K',
+    '기업 L',
+    '기업 M',
+    '기업 N',
+    '기업 O',
+    '기업 P',
+];
+
+const matchingTwo = () => {};
+
+const choiceWinner = () => {};
+
+const eight = [];
+const four = [];
+const two = [];
+</script>
+
+<style scoped></style>
