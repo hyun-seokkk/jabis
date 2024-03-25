c9ad117820d36678949e0c50536a3771d04be12c
정준수 <junsjung137@gmail.com>
2024-03-25 14:17:36 +0900

    Feat: Add news Controller, Entity, Service, Repo, Response, Mapper
    - 2024-03-25
    - NewsController, News, NewsService, NewsRepository, NewsResponse, NewsDtoMapper,

--- backend/src/main/java/com/ssafy/domain/news/entity/News.java
+++ backend/src/main/java/com/ssafy/domain/news/entity/News.java
@@ -5,7 +5,10 @@ import com.ssafy.domain.company.entity.Company;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotNull;
 
+import java.util.Map;
+
 import lombok.AccessLevel;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -40,5 +43,21 @@ public class News {
     @NotNull
     @Column(name = "link", columnDefinition = "TEXT")
     private String link;
+
+    /**
+     * 연관관계
+     */
+    private void setCompany(final Company company) {
+        this.company = company;
+    }
+
+    @Builder
+    public News(final Map<String, String> newsMap, final Company company) {
+        setCompany(company);
+        this.title = newsMap.get("title");
+        this.content = newsMap.get("content");
+        this.date = newsMap.get("date");
+        this.link = newsMap.get("link");
+    }
 
 }
