1c2f23d00025dfd760b7a7c1f148d69947225194
정준수 <junsjung137@gmail.com>
2024-04-02 16:30:23 +0900

    !HOTFIX: hotfix fastapi
    - 2024-04-02
    - main, naver_scrapper

--- fastapi/main.py
+++ fastapi/main.py
@@ -8,6 +8,7 @@ from fastapi.middleware.cors import CORSMiddleware
 
 import logging
 import time
+import re
 
 logger = logging.getLogger(__name__)
 logging.basicConfig(filename='news.log', level=logging.INFO)
@@ -35,13 +36,18 @@ def call_perform_news_search(name: Optional[str] = None):
     result = {}
 
     try:
-        result = call_openapi(perform_news_search(name, lastpage, MAX_ARTICLES))
+        result = call_openapi(perform_news_search(normalize_company_name(name), lastpage, MAX_ARTICLES))
         logger.info("Finished At: " + time.strftime('%c', time.localtime(time.time())))
 
     except Exception as e:
         logger.info(f"({time.localtime(time.time())})ERROR: {str(e)}")
 
     return result
+
+def normalize_company_name(company_name):
+    company_name = re.sub(r'\(.*?\)|주식회사|㈜|\(주\)|\s*Co\.?\s*Ltd\.?', '', company_name, flags=re.IGNORECASE)
+    company_name = company_name.strip().strip('"').strip("'")
+    return company_name
 
 if __name__ == '__main__':
     call_perform_news_search()
